56fb6b39d83b608a6979d96acc2655cc032e418c
Szymon Wojtkiewicz <swojtkiewicz@vp.pl>
2021-10-17 22:55:11 +0200

    2255 17.10.2021

--- src/main/java/My2048fx/Main2048.java
+++ src/main/java/My2048fx/Main2048.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main2048 extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("2048");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module My2048 {
     requires javafx.graphics;
+    exports My2048fx;
 }
